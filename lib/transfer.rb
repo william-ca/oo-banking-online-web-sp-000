ee67d1b9e53518326a0e4ce40eda730d54b92ffc
Will Andre <wandre94@gmail.com>
2021-02-01 23:45:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,14 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    else
+      "failed"
+    end
+  end
+
 end
