83256855f7c3fbb03cff82b6979b060aed8efecc
Will Andre <wandre94@gmail.com>
2021-02-01 23:41:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-      if self.status == "pending" && sender.balance > amount
+      if self.status == "pending" && sender.balance > amount && self.valid?
         sender.balance -= amount
         receiver.balance += amount
         self.status = "complete"
